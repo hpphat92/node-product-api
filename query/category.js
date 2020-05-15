c6a8bdbd66ad36561eceef632cf9ada0504149cf
Hong Phung Phat <phathongp@finantix.com>
2020-05-15 22:42:44 +0700

    Add category API

--- query/category.js
+++ query/category.js
@@ -3,10 +3,29 @@ const knex = require('../db/knex');
 
 const TABLE_NAME = 'category';
 
+function convertNameToSlug(str, separator) {
+  str = str
+    .toLowerCase()
+    .replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a')
+    .replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e')
+    .replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+    .replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o')
+    .replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u')
+    .replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+    .replace(/đ/g, 'd')
+    .replace(/\s+/g, '-')
+    .replace(/[^A-Za-z0-9_-]/g, '')
+    .replace(/-+/g, '-');
+  if (separator) {
+    return str.replace(/-/g, separator);
+  }
+  return str;
+}
 
-function getCategories() {
+function getCategories({ orderBy = 'name', orderDirection = 'asc' } = {}) {
   return knex(TABLE_NAME)
-    .select();
+    .select()
+    .orderBy(orderBy, orderDirection);
 }
 
 function getCategoryById(id) {
@@ -18,12 +37,46 @@ function getCategoryById(id) {
     .first();
 }
 
-function createCategory(model) {
+async function createCategory(model) {
+  model.slug = model.slug || convertNameToSlug(model.name);
+
+  const duplicateCategories = await knex(TABLE_NAME)
+    .where({
+      slug: model.slug
+    });
+
+  if (duplicateCategories && duplicateCategories.length) {
+    model.slug += Date.now();
+  }
   return knex(TABLE_NAME).insert(model);
+}
+
+function updateCategory(model) {
+  return knex(TABLE_NAME)
+    .where({ id: model.id })
+    .update(model);
+}
+
+function checkNameExisting(categoryName, ignoreCategoryId) {
+  let queryBuilder = knex(TABLE_NAME)
+    .where({
+      name: categoryName
+    });
+  if (ignoreCategoryId) {
+    queryBuilder = queryBuilder
+      .whereNot({
+        id: ignoreCategoryId
+      })
+  }
+
+
+  return queryBuilder;
 }
 
 module.exports = {
   getCategories,
   createCategory,
-  getCategoryById
+  updateCategory,
+  getCategoryById,
+  checkNameExisting
 };
--- routes/category.js
+++ routes/category.js
@@ -3,15 +3,25 @@ var query = require('../query/category');
 
 var router = express.Router();
 
+async function checkCategoryExisting(res, name, ignoreCategoryId) {
+  const categories = await query.checkNameExisting(name, ignoreCategoryId);
+
+  // Check duplicate
+  if (categories && categories.length) {
+    res.status(400).send('Category name exists');
+  }
+}
+
+
 // define the home page route
-router.get('/list', function (req, res) {
-  query.getCategories()
+router.get('/', function (req, res) {
+  query.getCategories(req.params)
     .then((response) => {
       res.status(200).send(response);
     })
 });
 // define the about route
-router.get('/detail/:id', function (req, res) {
+router.get('/:id', function (req, res) {
   const id = req.param('id');
   query.getCategoryById(id)
     .then((response) => {
@@ -21,5 +31,31 @@ router.get('/detail/:id', function (req, res) {
       console.log('err', err);
     })
 });
+router.put('/', async function (req, res) {
+  const body = req.body;
 
+  await checkCategoryExisting(res, body.name, body.id);
+
+  query.updateCategory(body)
+    .then((response) => {
+      res.sendStatus(200);
+    }, (err) => {
+      res.status(400).send('Error during updating category');
+    })
+
+});
+router.post('/', async function (req, res) {
+  const body = req.body;
+
+  await checkCategoryExisting(res, body.name);
+
+  query.createCategory(body)
+    .then((response) => {
+      console.log('response', response);
+      res.status(200).send(response);
+    }, (err) => {
+      res.status(400).send('Error during creating category');
+    })
+
+});
 module.exports = router;
